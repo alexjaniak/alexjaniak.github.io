2295588539c27fc64e43e65c1fe01255ec861a74
alexjaniak <alexander.janiak.acc@outlook.com>
2023-06-26 23:15:51 -0400

    added typed effect to home page

--- script.js
+++ script.js
@@ -1,18 +1,42 @@
-$(document).ready(function() {
-    // Initial scaling
-    scaleElement();
-  
-    // Scale element on window resize
-    $(window).resize(function() {
-      scaleElement();
-    });
-  });
-  
-  function scaleElement() {
-    // Calculate the scaling factor based on window width
-    var windowWidth = $(window).width();
-    var scaleFactor = windowWidth / 5000; // Adjust the value as needed
-  
-    // Apply the scaling to the element
-    $('.resize').css('transform', 'scale(' + scaleFactor + ')');
-  }
+document.addEventListener('DOMContentLoaded', function() {
+    let typewriters = document.getElementsByClassName('typed');
+
+    for (let i = 0; i < typewriters.length; i++) {
+        typeText(typewriters[i]);
+    }
+
+    function typeText(typewriter) {
+        let childNodes = typewriter.childNodes;
+        let textNodes = [];
+
+        for (let i = 0; i < childNodes.length; i++) {
+            let childNode = childNodes[i];
+            if (childNode.nodeType === Node.TEXT_NODE) {
+                textNodes.push(childNode);
+            }
+        }
+
+        for (let i = 0; i < textNodes.length; i++) {
+            typeNodeText(textNodes[i]);
+        }
+    }
+
+    
+    function typeNodeText(node) {
+        let text = node.textContent;
+        node.textContent = ''; // Clear existing text
+
+        let index = 0;
+
+        function type() {
+            node.textContent += text.charAt(index);
+            index++;
+            if (index < text.length) {
+                var delay = Math.random() * 50 + 20;
+                setTimeout(type, delay);
+            }
+        }
+
+        type();
+    }
+});
